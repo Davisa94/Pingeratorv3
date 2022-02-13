bbd8d3fc0e5bd6abb4e66f50ad96d2706a2ea6dc
Davisa94 <Austin.i.davis94@gmail.com>
2022-02-13 02:24:53 -0800

    Add project files.

---
+++ PingWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pingeratorv3
+{
+    class PingWrapper
+    {
+        //Given a remote host, attempt to ping it and return the given result
+        public static PingReply PingRemoteHost(string ipORDomain)
+        {
+            PingReply reply = null;
+            //try to ping the host
+            try 
+            {
+                var pinger = new Ping();
+                reply = pinger.Send(ipORDomain);
+                var  response = reply;
+                Console.WriteLine(response);
+
+            }
+            catch(PingException)
+            {
+
+            }
+            return reply;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pingeratorv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PingWrapper.PingRemoteHost("8.8.8.8").RoundtripTime);
+            Console.WriteLine(PingWrapper.PingRemoteHost("8.8.8.8").Address);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
